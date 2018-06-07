3dfdc859a0170fc97c8d8997922c9d5451b65bf5
Joven <jovensrai@gmail.com>
2018-06-07 18:46:30 +0000

    refactored to add foreach

---
+++ callbacks-array.js
@@ -0,0 +1,15 @@
+// The second argument/parameter is expected to be a function
+function findWaldo(arr, found) {
+  arr.forEach(function(name) {
+    if (name === "Waldo") {
+      found(arr.indexOf(name));   // execute callback
+    }
+  });
+}
+
+
+function actionWhenFound(index) {
+  console.log("Found him at index " + index + "!");
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
